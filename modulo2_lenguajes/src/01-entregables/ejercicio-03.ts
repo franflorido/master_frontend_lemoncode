e23b052827233fc41d7328c4a54171e658b94540
Francisco Florido <franciscoflorido11@gmail.com>
2024-02-21 17:00:01 +0100

    Adding different way to solve exercice 3

--- modulo2_lenguajes/src/01-entregables/ejercicio-03.ts
+++ modulo2_lenguajes/src/01-entregables/ejercicio-03.ts
@@ -1,19 +1,36 @@
 console.log("************** DELIVERABLE 03 *********************");
 
-// Clone
 function clone(source) {
-  return { ...source };
+  let result = {};
+  for (const key in source) {
+    result[key] = source[key];
+  }
+  return result;
 }
 
 console.log(clone({ name: "Maria", surname: "Ibañez" }));
 
-// Merge
+function merge(source, target) {
+  return { ...clone(target), ...clone(source) };
+}
+
 const a = { name: "Maria", surname: "Ibañez", country: "SPA" };
 const b = { name: "Luisa", age: 31, married: true };
+console.log(merge(a, b));
 
-function merge(source, target) {
+/// FORMA MAS FACIL ??
+
+// Clone
+function clone2(source) {
+  return { ...source };
+}
+
+console.log(clone2({ name: "Maria", surname: "Ibañez" }));
+
+// Merge
+function merge2(source, target) {
   let result = { ...target, ...source };
   return result;
 }
 
-console.log(merge(a, b));
+console.log(merge2(a, b));
