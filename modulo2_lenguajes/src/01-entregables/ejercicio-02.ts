0784cd3e4a53d87f43efa88acbd1671af12a73cf
Francisco Florido <franciscoflorido11@gmail.com>
2024-02-21 15:27:06 +0100

    Adding last minor modifications to exercices

--- modulo2_lenguajes/src/01-entregables/ejercicio-02.ts
+++ modulo2_lenguajes/src/01-entregables/ejercicio-02.ts
@@ -3,7 +3,9 @@ console.log("************** DELIVERABLE 02 *********************");
 const concat = <E>(a: Array<E>, b: Array<E>): Array<E> => {
   return [...a, ...b];
 };
-console.log("Resultado de Concat: ", concat([0, 1, 2], [3, 4, 5]));
+console.log("Resultado de Concat: ", concat([0, 1, 2], [3, 4, 5, 6, 7]));
+
+// OPTIONAL Excercice
 
 const multipleConcat = <E>(...collection: Array<Array<E>>): Array<E> => {
   let result: Array<E> = [];
--- modulo2_lenguajes/src/01-entregables/ejercicio-04.ts
+++ modulo2_lenguajes/src/01-entregables/ejercicio-04.ts
@@ -19,8 +19,11 @@ const books: Array<Books> = [
   { title: "Harry Potter y la piedra filosofal", isRead: true },
   { title: "Canción de hielo y fuego", isRead: false },
   { title: "Devastación", isRead: true },
+  { title: "ACOTAR", isRead: true },
 ];
 
 console.log(isBookRead(books, "Devastación")); // true
 console.log(isBookRead(books, "Canción de hielo y fuego")); // false
 console.log(isBookRead(books, "Los Pilares de la Tierra")); // false
+console.log(isBookRead(books, "ACOTAR")); // true
+console.log(isBookRead(books, "TOG")); // flase
